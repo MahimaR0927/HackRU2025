438f5842e379a888a693fcfb031a6f56d4476b1a
Hannah Vadakken <hannahvadakken@gmail.com>
2025-10-04 18:59:47 -0400

    idk2

--- flask-app/app.py
+++ flask-app/app.py
@@ -8,6 +8,10 @@ def index():
     return render_template("src/homepage.html")
 
 @app.route("/wardrobe", methods=['GET', 'POST'])
+
 def wardrobe():
-    #main.generate_tags()
     return render_template("src/wardrobe.html")
+
+
+def addTags(image_path):
+    return main.generate_tags(image_path)
