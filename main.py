41ae518df20bc1cebc15334b3ba8ad6fd3fb1acd
Hannah Vadakken <hannahvadakken@gmail.com>
2025-10-04 14:07:50 -0400

    added .env so we don't flash the key

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from google import genai
 from google.genai import types
 
+from dotenv import load_dotenv
+import os
+
 import requests
+
+load_dotenv()
 
 image_path = "https://goo.gle/instrument-img"
 image_bytes = requests.get(image_path).content
@@ -9,7 +14,9 @@ image = types.Part.from_bytes(
   data=image_bytes, mime_type="image/jpeg"
 )
 
-client = genai.Client(api_key ="AIzaSyCgg-000gchPm6qrgxZEkk0l_gJKgrk7qM")
+
+key = os.getenv("GEMINI_API_KEY")
+client = genai.Client()
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
