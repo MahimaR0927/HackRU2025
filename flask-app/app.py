fcbdedb8275c6d00e3346fcda2a097bdc7544d03
Hannah Vadakken <hannahvadakken@gmail.com>
2025-10-04 21:56:17 -0400

    app adjustment

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, url_for, redirect, render_template
 import main
 
@@ -12,24 +13,49 @@ def index():
 def wardrobe():
 
     imgs = []
-    error = ""
 
     try:
+        print(request.method == "POST")
         if request.method == "POST":
-            img_path = request.form.get("clothing-file").strip()
-            new_tags = main.generate_tags(img_path)
-            imgs.append(clothingPiece(img_path, new_tags))
 
+            if 'clothing-file' not in request.files:
+                print('No file part')
+
+            UPLOAD_FOLDER = 'templates/src/images' # Replace with your desired path
+            app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+            file = request.files['clothing-file']
+
+            # If the user does not select a file, the browser submits an empty file without a filename.
+            if file.filename == '':
+                print('No selected file')
+
+            if file:
+                filename = file.filename
+                
+                # This is the key step: Save the image data to your server's file system
+                file_path_on_server = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(file_path_on_server)
+                
+                # Now you have the file and its path on the server
+                print(f'File uploaded successfully to: {file_path_on_server}')
+                imgs.append(clothingPiece(filename, main.generate_tags(file_path_on_server)))
+
+        
     except Exception as e:
-        error = "Error:" + str(e)
+        print(str(e))
         imgs = []
 
-    return render_template("src/wardrobe.html", imgs = imgs)
+    return render_template("src/wardrobe.html", imgs = imgs)    
+
 
 
 class clothingPiece:
     def __init__(self, *args):
         self.img_path = args[0]
         self.tags = args[1]
+
+    def __str__(self):
+        return self.img_path + ":" + self.imgs
 
     
